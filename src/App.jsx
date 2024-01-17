c5b287d1b44e93efa6a4f10b726e90a930b5adeb
harshil-kakdiya <harshilpatel0334@gamil.com>
2024-01-17 15:57:42 +0530

    v.1.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas,  } from "./components";
 import SocialLinks from "./components/SocialLinks";
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -21,7 +21,7 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
